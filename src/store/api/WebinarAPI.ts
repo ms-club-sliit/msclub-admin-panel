749dfe1e751793a467e245b4725f15c749e8adf3
miyurugunarathna <33316556+miyurugunarathna@users.noreply.github.com>
2021-12-25 12:09:28 +0530

    Implemented Webinar APIs

---
+++ src/store/api/WebinarAPI.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { IWebinar } from "../webinar-store/IWebinar";
+import requestConfig from "./config";
+
+const BASE_URL = process.env.REACT_APP_API_ENDPOINT as string;
+
+class WebinarAPI {
+    static createWebinar(webinarData: IWebinar): Promise<IWebinar> {
+        return axios.post(`${BASE_URL}/admin/webinar/`, webinarData, requestConfig);
+    }
+
+    static getWebinar(webinarId: string): Promise<IWebinar> {
+        return axios.get(`${BASE_URL}/admin/webinar/${webinarId}`);
+    }
+
+    static getWebinars(): Promise<IWebinar[]> {
+        return axios.get(`${BASE_URL}/admin/webinar/`, requestConfig);
+    }
+
+    static getDeletedWebinars(): Promise<IWebinar[]> {
+        return axios.get(`${BASE_URL}/admin/webinar/delete/`, requestConfig);
+    }
+
+    static updateWebinar(webinarId: string, data: FormData): Promise<IWebinar> {
+        return axios.put(`${BASE_URL}/admin/webinar/${webinarId}`, data, requestConfig);
+    }
+
+    static deleteWebinar(webinarId: string): Promise<IWebinar> {
+        return axios.put(`${BASE_URL}/admin/webinar/delete/${webinarId}`, null, requestConfig);
+    }
+}
+
+export default WebinarAPI;
