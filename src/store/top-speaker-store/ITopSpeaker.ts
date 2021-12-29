30dd634fd5ce97d4e23f382e13b7a18b09d5a169
Shehan Bartholomeusz <it20274702@my.sliit.lk>
2021-12-29 21:42:44 +0530

    ITopSpeaker added

--- src/store/top-speaker-store/ITopSpeaker.ts
+++ src/store/top-speaker-store/ITopSpeaker.ts
@@ -1,1 +1,15 @@
-export{}
+import { IModifiedBy } from "../interfaces";
+
+interface ITopSpeaker {
+    _id?: string;
+    name: string;
+    title: string;
+    company: string;
+    imageUrl: string;
+    createdBy: IModifiedBy;
+    createdAt: Date;
+    updatedBy: IModifiedBy[];
+    updatedAt: Date;
+}
+
+export type { ITopSpeaker };
