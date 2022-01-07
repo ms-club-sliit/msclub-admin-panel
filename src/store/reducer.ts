f6c72f207635e01021a79b0152c7037cb9e3e821
miyurugunarathna <33316556+miyurugunarathna@users.noreply.github.com>
2022-01-08 02:26:09 +0530

    Exported Application reducer

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 import { combineReducers } from "redux";
 import userReducer from "./user-store/userReducer";
 import eventReducer from "./event-store/eventReducer";
 import applicationReducer from "./application-store/applicationReducer";
+=======
+import { combineReducers } from 'redux';
+import userReducer from './user-store/userReducer';
+import eventReducer from './event-store/eventReducer';
+import applicationReducer from './application-store/applicationReducer';
+>>>>>>> 705ae90 (Exported Application reducer)
 
 const reducers = combineReducers({
   userReducer,
