98b4b44619ca26238f86b0a295283e2ed1f5eee2
miyurugunarathna <33316556+miyurugunarathna@users.noreply.github.com>
2022-01-08 03:10:31 +0530

    Reducer rebase

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import { combineReducers } from "redux";
 import userReducer from "./user-store/userReducer";
 import eventReducer from "./event-store/eventReducer";
 import applicationReducer from "./application-store/applicationReducer";
-=======
-import { combineReducers } from 'redux';
-import userReducer from './user-store/userReducer';
-import eventReducer from './event-store/eventReducer';
-import applicationReducer from './application-store/applicationReducer';
->>>>>>> 705ae90 (Exported Application reducer)
 
 const reducers = combineReducers({
   userReducer,
