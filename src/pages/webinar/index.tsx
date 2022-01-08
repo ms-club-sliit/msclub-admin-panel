8d64376bdf8381ead50b8d735b9ffa52c4871d53
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-01-08 19:57:01 +0530

    implement the webinar form

--- src/pages/webinar/index.tsx
+++ src/pages/webinar/index.tsx
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from "react";
 import RichTextEditor from "react-rte";
+import ImageCanvas from "../../components/image-canvas";
+import { ToolBarConfig } from "../../constants";
 import {useDispatch, useSelector} from "react-redux";
 import { ToolBarConfig } from "../../constants";
 import { createWebinar, getWebinars } from "../../store/webinar-store/webinarActions";
@@ -50,12 +52,12 @@ const AddWebinar: React.FC = () => {
     useEffect(() =>{
         dispatch(getWebinars());
         closeModal();
-    }, [state.addWebinar, dispatch]); //  ******* //
+    }, [state.addWebinar, dispatch]);
 
     const closeModal = () =>{
         setState({...initialState});
         setEditor(RichTextEditor.createEmptyValue());
-        $("#addWebinarModel").modal("hide");
+        $("#addWebinarModal").modal("hide");
     };
 
     const onChange = (webinar: any) => {
@@ -140,7 +142,7 @@ const AddWebinar: React.FC = () => {
                 webinarFormData.append("registrationLink", registrationLink as string);
                 webinarFormData.append("webinarType", webinarType as string);
 
-                dispatch(createWebinar(webinarFormData)); // *******//
+                dispatch(createWebinar(webinarFormData));
             }
             else {
                 setState((prevState) => ({...prevState, isFormValid: true}));
@@ -148,8 +150,179 @@ const AddWebinar: React.FC = () => {
         }
     };
 
-    return ();
+    return (
+        <div
+        className="modal fade"
+        id="addWebinarModal"
+        tabIndex={-1}
+        data-mdb-backdrop="static"
+        data-mdb-keyboard="false"
+        aria-labelledby="addWebinarModalLabel"
+        aria-hidden="true"
+        >
+            <div className="modal-dialog modal-lg">
+                <div className="modal-content">
+                    <div className="modal-header">
+                    <h5 className="modal-title" id="addWebinarModalLabel">
+                        Add New Webinar
+                    </h5>
+                    <button type="button" className="btn-close" onClick={closeModal}></button>
+                    </div>
+                    <div className="modal-body add-event">
+                        <ImageCanvas width={300} height={300} getEditedImage={handleImage} />
+                        <div className="d-flex justify-content-center">
+                            {formData.imageUrl === null && isFormNotValid ? (
+                                <span className="text-danger validation-message my-2">Webinar flyer is required</span>
+                            ) : null}
+                        </div>
 
+                        <div className="mx-5">
+                            <div className="form-group row my-3">
+                                <label className="col-sm-3 col-form-label form-label text-dark">
+                                    <i className="far fa-file-alt fa-sm" />
+                                    &nbsp;Webinar Title
+                                </label>
+                                <div className="col-sm-9">
+                                    <input
+                                        type="text"
+                                        name="WebinarName"
+                                        value={title as string}
+                                        className="form-control"
+                                        onChange={onChange}
+                                    />
+                                    {formData.title === null && isFormNotValid ? (
+                                        <span className="text-danger validation-message">Webinar title is required</span>
+                                    ) : null}
+                                    </div>
+                                </div>
+                        </div>
+
+                        <div className="form-group row my-3">
+                            <label className="col-sm-3 col-form-label form-label text-dark">Webinar Type</label>
+                            <div className="col-sm-9">
+                            <select className="form-control" name="WebinarType" value={webibarType as string} onChange={onChange}>
+                                <option selected>Select Webinar type</option>
+                                <option value="PAST">PAST</option>
+                                <option value="UPCOMING">UPCOMING</option>
+                            </select>
+                            {formData.webinarType === null && isFormNotValid ? (
+                                <span className="text-danger validation-message">Webinar type is required</span>
+                            ) : null}
+                            </div>
+                        </div>
+
+                        <div className="form-group row my-3">
+                            <label className="col-sm-3 col-form-label form-label text-dark">
+                            <i className="far fa-clock fa-sm" />
+                            &nbsp;Date & Time
+                            </label>
+                            <div className="col-sm-9">
+                            <input
+                                type="datetime-local"
+                                id="dateTime"
+                                name="dateTime"
+                                value={dateTime as string}
+                                className="form-control"
+                                onChange={onChange}
+                            />
+                            {formData.dateTime === null && isFormNotValid ? (
+                                <span className="text-danger validation-message">Date & time is required</span>
+                            ) : null}
+                            </div>
+                        </div>
+
+                        <div className="form-group row my-3">
+                            <label className="col-sm-3 col-form-label form-label text-dark">
+                            <i className="fas fa-link fa-sm" />
+                            &nbsp;Registration Link
+                            </label>
+                            <div className="col-sm-9">
+                            <input
+                                type="text"
+                                className="form-control"
+                                name="registrationLink"
+                                value={registrationLink as string}
+                                onChange={onChange}
+                            />
+                            {formData.registrationLink === null && isFormNotValid ? (
+                                <span className="text-danger validation-message">Registration link is required</span>
+                            ) : null}
+                            </div>
+                        </div>
+
+                        <div className="form-group row my-3">
+                            <label className="col-sm-3 col-form-label form-label text-dark">
+                            <i className="fas fa-link fa-sm" />
+                            &nbsp;Webinar Link
+                            </label>
+                            <div className="col-sm-9">
+                            <input
+                                type="text"
+                                className="form-control"
+                                name="WebinarLink"
+                                value={link as string}
+                                onChange={onChange}
+                            />
+                            {formData.link === null && isFormNotValid ? (
+                                <span className="text-danger validation-message">Webinar link is required</span>
+                            ) : null}
+                            </div>
+                        </div>
+
+                        <div className="form-group row my-3">
+                            <label className="col-sm-3 col-form-label form-label text-dark">
+                            <i className="fas fa-tags fa-sm" />
+                            &nbsp;Tags
+                            </label>
+                            <div className="col-sm-9">
+                            <input
+                                type="text"
+                                className="form-control"
+                                value={filteredTags as string[]}
+                                onChange={(e) => handleTags(e.target.value)}
+                            />
+                            <small className="text-muted tag-text">
+                                Sperate tag names using , (example: ITP, GitHub, Microservice)
+                            </small>
+                            <br />
+                            {formData.filteredTags === null && isFormNotValid ? (
+                                <span className="text-danger validation-message">Tags are is required</span>
+                            ) : null}
+                            </div>
+                        </div>
+
+                        <div className="form-group row my-3">
+                            <label className="col-sm-12 col-form-label form-label text-dark">
+                            <i className="fas fa-align-left" />
+                            &nbsp;Description
+                            </label>
+                            <div className="col-sm-12">
+                            <RichTextEditor
+                                value={editor}
+                                onChange={handleDescription}
+                                toolbarClassName="description"
+                                editorClassName="description"
+                                toolbarConfig={ToolBarConfig}
+                            />
+                            {formData.description === null && isFormNotValid ? (
+                                <span className="text-danger validation-message">Description is required</span>
+                            ) : null}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-light shadow-none btn-rounded" onClick={closeModal}>
+                        Cancel
+                        </button>
+                        <button type="button" className="btn btn-primary shadow-none btn-rounded" onClick={onSubmit}>
+                        Submit
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default AddWebinar;
