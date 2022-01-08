1121fe9e8269af551d35f9fee72303a77fb8d626
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-01-08 15:38:28 +0530

    create AddWebinar function

--- src/pages/webinar/index.tsx
+++ src/pages/webinar/index.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import RichTextEditor from "react-rte";
 import {useDispatch, useSelector} from "react-redux";
 import { ToolBarConfig } from "../../constants";
-import { createWebinar } from "../../store/webinar-store/webinarActions";
+import { createWebinar, getWebinars } from "../../store/webinar-store/webinarActions";
 import { IwebinarFormData, IWebinarState } from "../../interfaces";
 
 let formData: IwebinarFormData ={
@@ -23,26 +24,132 @@ const initialState: IWebinarState = {
     imageUrl: "",
     dateTime: "",
     tags: [],
-    link: [],
+    link: "",
     registrationLink: "",
     webinarType: "",
 };
 
 const AddWebinar: React.FC = () => {
     const dispatch = useDispatch();
-    const state = useSelector((state) => state.eve)
-}
+    const state = useSelector((state) => state.webinarReducer);
+    const [editor, setEditor] = useState(() => RichTextEditor.createEmptyValue());
+    const [
+        {
+            title,
+            description,
+            imageUrl,
+            dateTime,
+            tags,
+            link,
+            registrationLink,
+            webinarType,
+        },
+        setState,
+    ] = useState(initialState);
 
+    useEffect(() =>{
+        dispatch(getWebinars());
+        closeModal();
+    }, [state.addWebinar, dispatch]); //  ******* //
 
-const index = () => {
+    const closeModal = () =>{
+        setState({...initialState});
+        setEditor(RichTextEditor.createEmptyValue());
+        $("#addWebinarModel").modal("hide");
+    };
 
+    const onChange = (webinar: any) => {
+        const {name, value} = webinar.target;
+        setState((prevState) => ({...prevState, [name]: value}));
+    };
 
+    const handleImage = (data: any) =>{
+        setState((prevState) => ({...prevState, imageUrl: data}));
+    };
 
-    return (
-        <div>
-            
-        </div>
-    )
-}
+    const handleDescription = (value: any) =>{
+        setEditor(value);
+        const isEmpty = !value.getEditorState().getCurrentContent().getPlainText().trim();
 
-export default index
+        if(isEmpty) {
+            setState((prevState) =>({
+                ...prevState,
+                description: null,
+            }));
+        }
+        else {
+            setState((prevState) =>({
+                ...prevState,
+                description: value.toString("html"),
+            }));
+        }
+    };
+
+    const handleTags = (value: string) => {
+        let tags = value.split(",");
+        let filterdTags: string[] = [];
+
+        if(tags.length > 0){
+            for(let tag of tags){
+                filterdTags.push(tag.trim());
+            }
+            console.log(filterdTags);
+            setState((prevState) => ({...prevState, filteredTags: filterdTags}));
+            console.log(filterdTags);
+        }
+    };
+
+    //Form validation
+    const validateForm = () =>{
+        const data = {
+            title: title && title.trim().length > 0 ? title : null,
+            description: description && description.trim().length > 0 ? description : null,
+            imageUrl: imageUrl ? imageUrl : null,
+            dateTime: dateTime && dateTime.trim().length > 0 ? dateTime : null,
+            tags: tags && tags.length > 0 ? tags : null,
+            link: link && link.trim().length > 0 ? link : null,
+            registrationLink: registrationLink && registrationLink.trim().length > 0 ? registrationLink : null,
+            webinarType: webinarType && webinarType.trim().length > 0 ? webinarType : null,
+        };
+
+        formData = Object.assign({}, data);
+        return true;
+    };
+
+    //Form Submission
+    const onSubmit = (webinar : any) =>{
+        webinar.preventDefault();
+
+        const isFormValid = validateForm();
+
+        if(isFormValid){
+            let data = Object.values(formData).map((item) =>{
+                return item !== null;
+            });
+
+            if(!data.includes(false)) {
+                setState((prevState) => ({...prevState, isFormValid: false}));
+
+                let webinarFormData = new FormData();
+                webinarFormData.append("webinarFlyer", imageUrl);
+                webinarFormData.append("title", title as string);
+                webinarFormData.append("description", description as string);
+                webinarFormData.append("dateTime", dateTime as string);
+                tags?.forEach((tag) => webinarFormData.append("tags", tag));
+                webinarFormData.append("link", link as string);
+                webinarFormData.append("registrationLink", registrationLink as string);
+                webinarFormData.append("webinarType", webinarType as string);
+
+                dispatch(createWebinar(webinarFormData)); // *******//
+            }
+            else {
+                setState((prevState) => ({...prevState, isFormValid: true}));
+            }
+        }
+    };
+
+    return ();
+
+};
+
+export default AddWebinar;
