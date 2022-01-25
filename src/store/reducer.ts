1f1ebf202ecd33ea3da3721b34b3af01e5904849
ShivaaniDushya <it20006884@my.sliit.lk>
2022-01-25 21:43:22 +0530

    Updated reducer.ts

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import userReducer from './user-store/userReducer';
 import eventReducer from './event-store/eventReducer';
+import topSpeakerReducer from './top-speaker-store/topSpeakerReducer';
 
 const reducers = combineReducers({
   userReducer,
   eventReducer,
+  topSpeakerReducer,
 });
 
 export type AppState = ReturnType<typeof reducers>;
