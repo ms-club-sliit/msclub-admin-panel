a3d0bd27dcbee9a09f8f913f02861bccbf7ad983
miyurugunarathna <33316556+miyurugunarathna@users.noreply.github.com>
2022-02-10 17:07:48 +0530

    Add user base add and delete function

--- src/pages/settings/users/index.tsx
+++ src/pages/settings/users/index.tsx
@@ -3,12 +3,21 @@ import { useDispatch, useSelector } from "react-redux";
 import { IUser } from "../../../interfaces";
 import { getAllUsers } from "../../../store/user-store/userActions";
 import AddUser from "./add";
+import DeleteUser from "./delete";
 import User from "./list";
+import ViewUser from "./view";
 
 const Users: React.FC = () => {
 	const dispatch = useDispatch();
 	const state = useSelector((state) => state.userReducer);
 	const [users, setUsers] = useState<IUser[]>();
+	const [permissionLevel, setPermissionLevel] = useState<string>();
+
+	useEffect(() => {
+		if (state.authUser && state.authUser.authToken && state.authUser.permissionLevel) {
+			setPermissionLevel(state.authUser.permissionLevel);
+		}
+	}, [state.authUser]);
 
 	useEffect(() => {
 		dispatch(getAllUsers());
@@ -20,16 +29,18 @@ const Users: React.FC = () => {
 
 	return (
 		<div className="organization">
-			<div className="d-flex justify-content-end">
-				<button
-					className="btn btn-primary btn-rounded shadow-none mb-2"
-					data-mdb-toggle="modal"
-					data-mdb-target="#addUserModal"
-				>
-					<span className="fas fa-plus" />
-					<span className="mx-2">New User</span>
-				</button>
-			</div>
+			{permissionLevel === "ROOT_ADMIN" && (
+				<div className="d-flex justify-content-end">
+					<button
+						className="btn btn-primary btn-rounded shadow-none mb-2"
+						data-mdb-toggle="modal"
+						data-mdb-target="#addUserModal"
+					>
+						<span className="fas fa-plus" />
+						<span className="mx-2">New User</span>
+					</button>
+				</div>
+			)}
 			<div className="row">
 				{users &&
 					users.length &&
@@ -48,7 +59,9 @@ const Users: React.FC = () => {
 						</div>
 					))}
 			</div>
-			<AddUser />
+			{permissionLevel === "ROOT_ADMIN" && <AddUser />}
+			<ViewUser />
+			{permissionLevel === "ROOT_ADMIN" && <DeleteUser />}
 		</div>
 	);
 };
