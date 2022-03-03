8870e0f44b451c75cec37a62b12247367b99ba31
rusiruavb <rusiruavb98@gmail.com>
2022-03-03 22:40:54 +0530

    Change the folder name from `dashboard_new` to `dashboard`

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -7,7 +7,7 @@ import ApplicationList from "./application/list";
 import DeletedWebinarList from "./webinar/list/delete";
 import InquiryList from "./inquiry/list";
 import DeletedInquiryList from "./inquiry/list/delete";
-import Dashboard from "./dashboard_new";
+import Dashboard from "./dashboard";
 import DeletedApplicationList from "./application/list/delete";
 import TopSpeakerList from "./top-speaker/list";
 import DeletedTopSpeakerList from "./top-speaker/list/delete";
